d116cd277516392f913bbc8bce523d3aa2a14f51
amalija-ramljak <amalija@netgen.io>
2022-04-10 12:36:39 +0200

    update sectiongroup

--- lib/components/Sections/SectionGroup.svelte
+++ lib/components/Sections/SectionGroup.svelte
@@ -1,18 +1,39 @@
 <script lang="ts">
   import { fade } from 'svelte/transition';
   import { cubicInOut as cubic } from 'svelte/easing';
-  import Section from './Section.svelte';
-  import Loader from '$slib/components/Loader.svelte';
 
-  export let sections = [];
+  import Loader from '$slib/components/Loader.svelte';
+  import Pager from '$slib/components/Filters/Pager.svelte';
+
+  export let sections: Array<any> = [];
+  export let noSections: boolean;
+  export let lastPage: boolean;
+  export let page: number = 1;
+
+  export let Section: any;
 </script>
 
 <div>
-  {#each sections as section, i}
+  {#each sections as { id, ...section }, i (id)}
     <div in:fade={{ easing: cubic, duration: 700, delay: i * 75 }}>
-      <Section {...section} />
+      <svelte:component this={Section} {...section} />
     </div>
   {:else}
-    <Loader />
+    {#if noSections}
+      <div class="empty-message">
+        <slot name="empty-list">No sections to display!</slot>
+      </div>
+    {:else}
+      <Loader />
+    {/if}
   {/each}
+
+  <Pager {page} showNext={!lastPage} />
 </div>
+
+<style>
+  .empty-message {
+    padding-left: 1rem;
+    padding-top: 2rem;
+  }
+</style>
